ca8e11e8f31f3c5ff60bdb74569a89a77fb97855
DanilKuleshov <90615841+DanilKuleshov@users.noreply.github.com>
2022-03-23 00:49:09 +0700

    Add files via upload

---
+++ day1/part1.py
@@ -0,0 +1,19 @@
+file = open("input.TXT", 'r')
+List = file.readline()
+op = 0
+cl = 0
+
+for i in range(0, len(List)):
+    if List[i] == '(':
+        op += 1
+    if List[i] == ')':
+        cl += 1
+resoult = op - cl
+print("Количество раз когда Санта поднимется на этаж: " + str(op))
+print("Количество раз когда Санта опустится на этаж: " + str(cl))
+print("Санта окажется на " + str(resoult) + " этаже.")
+file.close()
+f = open("output1.TXT", 'w')
+g = str(resoult)
+f.write(g)
+f.close()
