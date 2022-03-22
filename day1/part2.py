92c68448156495f15e5b502de155d95430fe1a76
DanilKuleshov <90615841+DanilKuleshov@users.noreply.github.com>
2022-03-23 01:29:16 +0700

    Add files via upload

---
+++ day1/part2.py
@@ -0,0 +1,21 @@
+file = open("input.TXT", 'r')
+List = file.readline()
+op = 0
+cl = 0
+tt = 0
+
+for i in range(0, len(List)):
+    tt +=1
+    if List[i] == '(':
+        op += 1
+    if List[i] == ')':
+        cl += 1
+    if op - cl == -1:
+        break
+resoult = op - cl
+print("Санта впервые впервые на " + str(resoult) + " этаже на " + str(tt) + " позиции.")
+file.close()
+f = open("output2.TXT", 'w')
+g = str(resoult)
+f.write(g)
+f.close()
