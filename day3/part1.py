cd51df29d02c9afa1ccaa8d9500dc4df60bbbd89
DanilKuleshov <90615841+DanilKuleshov@users.noreply.github.com>
2022-03-26 01:10:27 +0700

    Update part1.py

--- day3/part1.py
+++ day3/part1.py
@@ -1,1 +1,28 @@
-.
+Lines = []
+with open('input.txt', 'r') as Input:
+    line = Input.readlines()
+    line = str(line)[2:-2]
+    Lines.append([0, 0])
+    y = 0
+    x = 0
+
+    for i in line:
+
+        if i == '>':
+            x += 1
+        elif i == '<':
+            x += -1
+        elif i == '^':
+            y += 1
+        elif i == 'v':
+            y += -1
+        Lines.append([x, y])
+    counts = Lines.copy()
+    for j in counts:
+        if Lines.count(j) > 1:
+            for k in range(Lines.count(j) - 1):
+                Lines.remove(j)
+print('Санта посетил ' + str(len(Lines)) + ' дома за ночь!')
+
+with open('output1.txt', 'w') as output:
+    output.write(str(len(Lines)))
